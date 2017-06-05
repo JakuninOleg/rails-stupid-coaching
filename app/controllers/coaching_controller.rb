c65735c3f4aeb96ae5cc3629b600001fa432fcae
Oleg Jakunin <oleg.kemz@gmail.com>
2017-06-05 11:54:36 +0200

    fixed css

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -4,8 +4,8 @@ class CoachingController < ApplicationController
   def answer
     @show_answer = params[:question]
 
-    @your_answer = params[:question].downcase
-    @coach_answer = Stupid_coaching.give_answer(@your_answer)
+    your_answer = params[:question].downcase
+    @coach_answer = Stupid_coaching.give_answer(your_answer)
   end
 
   def ask
--- lib/stupid_coaching.rb
+++ lib/stupid_coaching.rb
@@ -1,4 +1,4 @@
-class Stupid_coaching
+class StupidCoaching
   def self.give_answer(your_answer)
     if your_answer.include?("oleg")
       "You god damn right!"
@@ -8,6 +8,8 @@ class Stupid_coaching
       "No underwear makes you stupid!"
     elsif your_answer.include?("francesco")
       "Il Padrino is super teacher, but not a student!"
+    elsif your_answer.include?("albert")
+      "Albert is good, but you know, there is one student..."
     else
       "Fake news! Try again!"
     end
